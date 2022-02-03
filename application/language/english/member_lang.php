3ab22d253e7e9a19716a8f8918c887bce68b3861
elyassgh <elghazi.elyass@gmail.com>
2022-02-03 16:28:04 +0100

    adding fr lang

--- application/language/english/member_lang.php
+++ application/language/english/member_lang.php
@@ -41,4 +41,4 @@ $lang['member_book_no']       = "No Ouvrage";
 $lang['member_issued']        = "Publié";
 $lang['member_return']        = "Retourner";
 $lang['member_lost']          = "Perdu";
-$lang['member_book_issue']    = "Numéro de livre";
+$lang['member_book_issue']    = "Numéro d'Ouvrage";
